5c2ded558e68c16ad6f5e6b77171dd4942a05433
Ragesh Punathil <rpc.ragesh@hotmail.com>
2024-01-28 14:44:54 +0530

    base code

---
+++ ui/src/app/global-component.ts
@@ -0,0 +1,22 @@
+export const GlobalComponent = {
+  // Api Calling
+  API_URL: ' https://localhost:7266/',
+  // API_URL : 'http://127.0.0.1:3000/',
+  headerToken: { 'Authorization': `Bearer ${localStorage.getItem('token')}` },
+
+  // Auth Api
+  AUTH_API: " https://localhost:7266/auth/",
+  // AUTH_API:"http://127.0.0.1:3000/auth/",
+
+
+  // Products Api
+  product: 'apps/product',
+  productDelete: 'apps/product/',
+
+  // Orders Api
+  order: 'apps/order',
+  orderId: 'apps/order/',
+
+  // Customers Api
+  customer: 'apps/customer',
+}
